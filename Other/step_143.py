64885086e6c3223d55494c81371c89a8e3edb60f
ckhilton <ckirkhilton@gmail.com>
2020-07-27 12:18:22 -0600

    Re-organize folders

---
+++ Other/step_143.py
@@ -0,0 +1,11 @@
+#------------------------------------
+#   Author: Kirk Hilton
+#   Assignment: Step 143
+#------------------------------------
+
+def printMe():
+    """This is my docstring"""
+
+    print("Meeeeeee!")
+
+print(printMe.__doc__)
