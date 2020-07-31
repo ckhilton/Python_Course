04647ec60adf2fdb2096665d97c8bda19096a2f5
ckhilton <ckirkhilton@gmail.com>
2020-07-31 06:01:18 -0600

    Update Assignment_175.py

--- Assignment_175/Assignment_175.py
+++ Assignment_175/Assignment_175.py
@@ -10,28 +10,28 @@ class Person:
         print("Hello there", self.firstname, self.lastname)
 
 class Graduate(Person):
-  def __init__(self, fname, lname, school, year):
-    super().__init__(fname, lname)
+  def __init__(self, fname, lname, school, year): #initialize
+    super().__init__(fname, lname) #use super() to inherit parent class methods and attributese
     self.collegeName = school
     self.graduationYear = year
-  def congrats(self):
+  def congrats(self): #create print function
     print("Congratulations", self.firstname, self.lastname, "on graduating from", self.collegeName, "in the year", self.graduationYear)
 
 class Teacher(Person):
-  def __init__(self, fname, lname, subject, years):
-    super().__init__(fname, lname)
+  def __init__(self, fname, lname, subject, years): #initialize
+    super().__init__(fname, lname) #use super() to inherit parent class methods and attributes
     self.teachingSubject = subject
     self.teachingYears = years
-  def thanks(self):
+  def thanks(self): #create print function
     print("Thank you", self.firstname, self.lastname, "for teaching", self.teachingSubject, "for the last", self.teachingYears, "years")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #control code
     x = Person("Joe", "Schmoe")
-    x.printname()
+    x.printname() #call and print instance of Person class
 
     y = Graduate("Kirk", "Hilton", "UCDH", 2014)
-    y.congrats()
+    y.congrats() #call and print instance of Graduate class
 
     z = Teacher("Mary", "Poppins", "acting", 30)
-    z.thanks()
+    z.thanks() #call and print instance of Teacher class
